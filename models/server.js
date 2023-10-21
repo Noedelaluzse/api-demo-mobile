f0f793b9146564d035e9272952a99cfa159ad82a
Noe de la luz <gilberto.delaluz@upnify.cc>
2023-10-20 19:58:21 -0500

    se agrega archivo publico

--- models/server.js
+++ models/server.js
@@ -6,7 +6,7 @@ class Server {
 
     constructor() {
         this.app = express();
-        this.port = process.env.PORT || 3000;
+        this.port = process.env.PORT;
         this.paths = {
             auth: '/api/auth',
             items: '/api/items',
